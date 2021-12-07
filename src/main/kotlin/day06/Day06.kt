ded7678782d6f2987a85807805a60156c5c076ca
Julian Kotrba <julian.kotrba@apa.at>
2021-12-07 16:06:35 +0100

    Solve day 6 part 2

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -3,37 +3,36 @@ package day06
 import parseFile
 
 fun main() {
-
-    val testInput = """3,4,3,1,2""".trimIndent().split(",").map { it.toInt() }
-
     val input = parseFile("src/main/kotlin/day06/input") { it }[0].split(",").map { it.toInt() }
 
-    println("Part 1: " + part1(testInput, 80))
-    println("Part 1: " + part1(input, 80))
-    println("Part 2: " + part1(input, 256)) // TODO: Way to slow for part 2
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
 }
 
-fun part1(input: List<Int>, days: Int): Int {
-    var mutableList: MutableList<Int> = input.toMutableList()
+fun part1(input: List<Int>) =
+        input.groupBy { it.toLong() }
+                .map {
+                    countFish(it.key, 80) * it.value.size
+                }
+                .sum() + input.size
 
-    repeat(days) {
 
-        val copy = mutableList
-
-        (0 until mutableList.size).forEach { index ->
-            val newValue = copy[index] - 1
-
-            if (newValue == -1) {
-                copy[index] = 6
-                copy.add(8)
-            } else {
-                copy[index] = newValue
-            }
+private fun part2(input: List<Int>) = input
+        .groupBy { it.toLong() }
+        .map {
+            countFish(it.key, 256) * it.value.size
         }
+        .sum() + input.size
 
-        mutableList = copy
+private fun countFish(n: Long, days: Int): Long {
+    var c = 0L
+
+    var tmpDays = days
+    var tmpN: Long = n
+    while (tmpDays - (tmpN + 1) >= 0L) {
+        tmpDays -= (tmpN.toInt() + 1)
+        c += (1 + countFish(8, tmpDays))
+        tmpN = 6
     }
-
-    return mutableList.size
+    return c
 }
-
