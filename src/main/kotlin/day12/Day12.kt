749cdeb82b55ebcc061b4fb5d295056acbf27237
Julian Kotrba <julian.kotrba@apa.at>
2021-12-12 20:46:56 +0100

    Solve day 12 part 2

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -6,58 +6,85 @@ fun main() {
 
     val input = parseFile("src/main/kotlin/day12/input") { it.map { Pair(it.split("-")[0], it.split("-")[1]) } }
 
-    val testInput = """
-                    fs-end
-                    he-DX
-                    fs-he
-                    start-DX
-                    pj-DX
-                    end-zg
-                    zg-sl
-                    zg-pj
-                    pj-he
-                    RW-he
-                    fs-DX
-                    pj-RW
-                    zg-RW
-                    start-pj
-                    he-WI
-                    zg-he
-                    pj-fs
-                    start-RW
-    """.trimIndent().lines().map {
-        Pair(it.split("-")[0], it.split("-")[1])
-    }
-
-    println("Part 1: " + part1(testInput))
     println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
 }
 
 fun part1(input: List<Pair<String, String>>): Int {
 
     val allPaths = input + input.map { Pair(it.second, it.first) }
-
     val mapped: Map<String, Set<String>> = allPaths.groupBy({ it.first }, { it.second }).mapValues { it.value.toSet() }
 
     val paths = mutableSetOf<List<String>>()
 
     mapped["start"]?.forEach {
-        paths(mapped, paths, mutableListOf("start"))
+        paths1(mapped, paths, mutableListOf("start"))
     }
 
     return paths.size
 }
 
-private fun paths(mappedInput: Map<String, Set<String>>, paths: MutableSet<List<String>>, path: MutableList<String>) {
+fun part2(input: List<Pair<String, String>>): Int {
+
+    val allPaths = input + input.map { Pair(it.second, it.first) }
+    val mapped: Map<String, Set<String>> = allPaths.groupBy({ it.first }, { it.second }).mapValues { it.value.toSet() }
+
+    val paths = mutableSetOf<List<String>>()
+
+    mapped["start"]?.forEach {
+        paths2(mapped, paths, mutableListOf("start"), false)
+    }
+
+    return paths.size
+}
+
+private fun paths1(mappedInput: Map<String, Set<String>>, paths: MutableSet<List<String>>, path: MutableList<String>) {
     mappedInput[path.last()]?.forEach {
-        if (it.allUpperCase() || !path.contains(it)) {
+        if (it.allUpperCase() || !path.contains(it)) { // TODO: Optimize contains check
             if (it == "start") return@forEach
             if (it == "end") {
                 paths.add(path.toMutableList().apply { add(it) })
             } else {
-                paths(mappedInput, paths, path.toMutableList().apply {
+                paths1(mappedInput, paths, path.toMutableList().apply {
                     add(it)
                 })
+            }
+        }
+    }
+}
+
+private fun paths2(mappedInput: Map<String, Set<String>>, paths: MutableSet<List<String>>, path: MutableList<String>, alreadyTwice: Boolean) {
+    mappedInput[path.last()]?.forEach {
+        // TODO: Restructure nesting
+        if (it.allUpperCase()) {
+
+            if (it == "start") return@forEach
+            if (it == "end") {
+                paths.add(path.toMutableList().apply { add(it) })
+            } else {
+                paths2(mappedInput, paths, path.toMutableList().apply {
+                    add(it)
+                }, alreadyTwice)
+            }
+        } else {
+            if (!path.contains(it)) {  // TODO: Optimize contains check
+                if (it == "start") return@forEach
+                if (it == "end") {
+                    paths.add(path.toMutableList().apply { add(it) })
+                } else {
+                    paths2(mappedInput, paths, path.toMutableList().apply {
+                        add(it)
+                    }, alreadyTwice)
+                }
+            } else if (path.contains(it) && !alreadyTwice) { // TODO: Optimize contains check
+                if (it == "start") return@forEach
+                if (it == "end") {
+                    paths.add(path.toMutableList().apply { add(it) })
+                } else {
+                    paths2(mappedInput, paths, path.toMutableList().apply {
+                        add(it)
+                    }, true)
+                }
             }
         }
     }
